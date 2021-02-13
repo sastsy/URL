519b7683e5c2e6dbc4dad16a2f662098d01e6884
sastsy <sas_tsy@mail.ru>
2021-02-13 17:57:14 +0400

    new

--- app/src/main/java/ru/samsung/itschool/book/MainActivity.java
+++ app/src/main/java/ru/samsung/itschool/book/MainActivity.java
@@ -4,11 +4,15 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
+import android.os.AsyncTask;
 import android.os.Bundle;
+import android.text.Layout;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ImageView;
+import android.widget.ProgressBar;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import java.io.IOException;
@@ -16,40 +20,51 @@ import java.io.InputStream;
 import java.net.URL;
 
 public class MainActivity extends AppCompatActivity {
-    Button start_button;
-    EditText textEdit;
-    ImageView imageView;
+    Button bStart, btJustDoIt;
+    ProgressBar progressBar;
+    TextView text;
+    EditText edit;
+    ImageView pic;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        start_button = (Button) findViewById(R.id.start_button);
-        textEdit = (EditText) findViewById(R.id.textEdit);
-        imageView = (ImageView) findViewById(R.id.image);
-        final Thread thread = new Thread(new LoadImage());
+        bStart = (Button) findViewById(R.id.btStart);
+        text = (TextView) findViewById(R.id.text);
+        edit = (EditText) findViewById(R.id.edit);
+        pic = (ImageView) findViewById(R.id.pic);
 
-        start_button.setOnClickListener(new View.OnClickListener() {
+        bStart.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Toast.makeText(MainActivity.this, "Делаем операцию в потоке", Toast.LENGTH_SHORT).show();
-                thread.start();
+                Toast.makeText(MainActivity.this, "Делаем операцию в потоке (10с)", Toast.LENGTH_SHORT).show();
+                new LoadImage().execute();
             }
         });
     }
+    private class LoadImage extends AsyncTask<Void, Integer, Bitmap> {
 
-    private class LoadImage implements Runnable {
         @Override
-        public void run() {
+        protected void onPreExecute() {
+            super.onPreExecute();
+        }
+
+        protected Bitmap doInBackground(Void... args) {
             try {
-                URL url = new URL(textEdit.getText().toString());
+                URL url = new URL(edit.getText().toString());
                 InputStream image = (InputStream) url.getContent();
-                Bitmap myImage = BitmapFactory.decodeStream(image);
-                imageView.setImageBitmap(myImage);
+                return BitmapFactory.decodeStream(image);
             }
             catch (IOException e) {
                 e.printStackTrace();
+                return null;
             }
         }
+
+        protected void onPostExecute(Bitmap image) {
+            pic.setImageBitmap(image);
+        }
+
     }
 }
